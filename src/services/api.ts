4b85f22a78d69c63beccc1cc9035f4f6628811db
IgorTargino <igortargino01@gmail.com>
2021-05-27 04:12:10 -0300

    fix: api

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ const ts = Number(new Date());
 const hash = md5(ts + process.env.PUBLIC_KEY + process.env.PRIVATE_KEY)
 
 const api = axios.create({
-  baseURL: 'https://gateway.marvel.com:443/v1/public/',
+  baseURL: 'https://gateway.marvel.com/v1/public/',
   params: {
     ts,
     apikey: process.env.PUBLIC_KEY,
