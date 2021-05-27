d1137ec8a4640e0c58a95994617c39f69f311209
IgorTargino <igortargino01@gmail.com>
2021-05-27 01:16:39 -0300

    feat: component header

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-    <div >
-      <h1>Hello World</h1> 
-    </div>
-  )
-}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+
+import styles from "./styles.module.scss";
+
+const Header = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [type, setType] = useState("");
+
+  return (
+    <header className={styles.container}>
+      <div className={styles.maxWidth}>
+        <div className={styles.info}>
+          <AiOutlineInfoCircle size={20} />
+        </div>
+        <div className={styles.logo}>
+          <img src="./logo.png" alt="logo da marvel" />
+        </div>
+
+        <div className={styles.search}>
+          <input
+            type="search"
+            placeholder="Spider Man"
+            value={inputValue}
+            onChange={(event) => setInputValue(event.target.value)}
+          />
+          <select value={type} onChange={(event) => setType(event.target.value)}>
+            <option selected value="characters">personagens</option>
+            <option value="stories">histórias</option>
+            <option value="series">series</option>
+          </select>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
