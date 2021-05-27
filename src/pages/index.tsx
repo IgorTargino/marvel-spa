8d6fe08f2954686f3c5d9404f4f4e69a7cc4a649
IgorTargino <igortargino01@gmail.com>
2021-05-27 03:38:53 -0300

    feat: http request

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import Header from '../components/Header'
-import styles from '../styles/Home.module.scss'
+import Header from "../components/Header";
+import styles from "../styles/Home.module.scss";
+
+import api from "../services/api";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    api
+      .get('characters/')
+      .then((response) => console.log(response.data.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className={styles.container}>
       <Header />
     </div>
-  )
+  );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from 'axios';
+import md5 from 'md5';
 
-export default axios.create({
-  baseURL: process.env.API_URL
+const ts = Number(new Date());
+const hash = md5(ts + process.env.PUBLIC_KEY + process.env.PRIVATE_KEY)
+
+const api = axios.create({
+  baseURL: 'https://gateway.marvel.com:443/v1/public/',
+  params: {
+    ts,
+    apikey: process.env.PUBLIC_KEY,
+    hash,
+  },
 });
+
+export default api;
